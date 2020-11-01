3bc9b3b907fac499b6248e8389ec50f91d10b27d
jwkuyper <72477571+jwkuyper@users.noreply.github.com>
2020-11-01 10:49:28 -0600

    .py app customized for our Group
    Postman: 
    http://127.0.0.1:5000/regression?iso_code=103&total_cases=32364&total_deaths=548&new_deaths=6&total_cases_per_million=601.882&new_cases_per_million=4.575&total_deaths_per_million=10.191&new_deaths_per_million=0.112&new_tests=3381&total_tests=425364&total_tests_per_thousand=7.911&new_tests_per_thousand=0.063&population=53771300&median_age=20&life_expectancy=66.7

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on 
 
-@author: 
-"""
 import os
 import pandas as pd
 
@@ -24,51 +20,11 @@ api = Api(app)
 #parser.add_argument('query')
 
 
-class ForecastTimeSeriesEts(Resource):
-    def get(self):
-        model_path = 'lib/models/timeSeriesModel.pkl'
-        with open(model_path, 'rb') as f:
-            ets_model = joblib.load(f)
-                
-        # get the query parameters
-        steps = int(request.args.get('steps'))
-        
-        # make a prediction
-        pred_uc = ets_model.forecast(steps)
-       
-        # create JSON object
-        output = pred_uc.to_json(orient='table')
-        output=json.loads(output)
-        
-        return output
-    
-class ForecastTimeSeriesSarima(Resource):
-    def get(self):
-        model_path = 'lib/models/timeSeriesModel.pkl'
-        with open(model_path, 'rb') as f:
-            sarima_model = joblib.load(f)
-                
-        # get the query parameters
-        steps = int(request.args.get('steps'))
-        
-        # make a prediction
-        pred_uc = sarima_model.get_forecast(steps=steps)
-        pred_ci = pred_uc.conf_int(alpha=.05)
-        pred_ci['Prediction'] = pred_uc.predicted_mean
-        pred_ci.columns=['Lower','Upper','Prediction']
-        
-        print(pred_ci)
-        print(type(pred_ci))
-       
-        # create JSON object
-        output = pred_ci.to_json(orient='table')
-        output=json.loads(output)
-        
-        return output
-  
+
+
 class PredictRegression(Resource):
     def get(self):
-        model_path = 'lib/models/lrteam1.pkl'
+        model_path = 'lrteam1.pkl'
         with open(model_path, 'rb') as f:
             regression_model = joblib.load(f)
                 
@@ -89,7 +45,7 @@ class PredictRegression(Resource):
         return output
     
     def post(self):
-        model_path = 'lib/models/lrteam1.pkl'
+        model_path = 'lrteam1.pkl'
         with open(model_path, 'rb') as f:
             regression_model = joblib.load(f)
                 
@@ -108,55 +64,9 @@ class PredictRegression(Resource):
         
         return output
 
-class PredictClassification(Resource):
-    def get(self):
-        model_path = 'lib/models/classificationModel.pkl'
-        with open(model_path, 'rb') as f:
-            classification_model = joblib.load(f)
-                
-       # get the query parameters
-        params = request.args
-
-        inputs=pd.DataFrame(params,index=[0])
-              
-        # make a prediction
-        predictions = classification_model.predict(inputs)
-       
-        # create JSON object
-        #pd.DataFrame(predictions, columns=['Prediction'])
-        output = pd.DataFrame(predictions, columns=['Prediction']).to_json(orient='table')
-        output=json.loads(output)
-        
-        return output
-    
-    def post(self):
-        model_path = 'lib/models/classificationModel.pkl'
-        with open(model_path, 'rb') as f:
-            classification_model = joblib.load(f)
-                
-       # get the form data
-        params = request.form
-
-        inputs=pd.DataFrame(params,index=[0])
-              
-        # make a prediction
-        predictions = classification_model.predict(inputs)
-       
-        # create JSON object
-        #pd.DataFrame(predictions, columns=['Prediction'])
-        output = pd.DataFrame(predictions, columns=['Prediction']).to_json(orient='table')
-        output=json.loads(output)
-        
-        return output
-
-
-
 # Setup the Api resource routing here
 # Route the URL to the resource
-api.add_resource(ForecastTimeSeriesEts, '/time_ets')
-api.add_resource(ForecastTimeSeriesSarima, '/time_sarima')
 api.add_resource(PredictRegression, '/regression')
-api.add_resource(PredictClassification, '/classification')
 
 
 if __name__ == '__main__':
